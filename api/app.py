b35a830a0c7d2d784f4d6a6da7c538b1ab0e2860
TK <teekayu8@hotmail.com>
2018-02-05 03:42:17 +0800

    Add CORS permission

--- api/app.py
+++ api/app.py
@@ -3,10 +3,12 @@
 Author: Teekayu Klongtruajrok
 """
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from .resources import Analysis
 
 app = Flask(__name__)
+cors = CORS(app, resources={"/api/*": {"origins": "*"}})
 api = Api(app)
 
 api.add_resource(Analysis, '/api/Analysis')
