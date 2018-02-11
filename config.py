d776784adce22b0b0da199fe19dc5776b47062bf
Teekayu Klongtruajrok <teekayu8@hotmail.com>
2018-02-11 22:54:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class BaseConfig:
     """Core configs"""
 
     TWITTER = {
-        'consumer_key': 'ZKXRTO132QyaycJw7ArBLO9gZ',
-        'consumer_secret': '2f8eKJgS8FFwW4ZYXtehs7djlcL26yhRpqjZYDsheUx28XQAXN',
-        'access_token_key': '55545272-qxTEC4ztSX7lNvqJGulKJruDWfBD0pSFUl9e4BwYL',
-        'access_token_secret': 'lzY7UQnscuV18ZmVDYG5rcooRAGzvaPtLwnMDvWQl4av9'
+        'consumer_key': '????',
+        'consumer_secret': '????',
+        'access_token_key': '????',
+        'access_token_secret': '????'
     }
